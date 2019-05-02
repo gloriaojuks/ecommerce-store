eea2675dee336486383f523a6abb855614635c31
PhreetechDev4 <gloriaojuks@gmail.com>
2019-05-02 15:02:44 +0100

    everything is set

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../logo.svg'
 import styled from 'styled-components'
+import {ButtonContainer} from './Button'
 
 export class Navbar extends Component {
   render() {
     return (
       <div>
-          <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+          <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
             { /* 
         https://www.iconfinder.com/icons/1243689/call_phone_icon
         Creative Commons (Attribution 3.0 Unported);
@@ -16,23 +17,20 @@ export class Navbar extends Component {
         </Link>
         <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5"></li>
-            <Link to="/" className="nav-link">Products</Link>
+            <Link to="/" className="nav-link">products</Link>
         </ul>
         <Link to="./cart" className="ml-auto">
             <ButtonContainer><span><i className="fas fa-cart-plus"/></span>My Cart </ButtonContainer>
         </Link>
-        </nav>
+        </NavWrapper>
       </div>
     )
   }
 }
 
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background: transparent;
-border: 0.05rem solid var(--lightBlue);
-
-`
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{color:var(--mainWhite)!important;font-size:1.3rem;text-transform: capitalize}
+`;
 
 export default Navbar
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title'
 
 export class ProductList extends Component {
+    state={
+        Products: []
+    }
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+          <div className="py-5">
+            <div className="container">
+            <Title name="our" title="products" />
+                <div className="row">
+                
+                </div>
+            </div>
+          </div>
+        {/* <Product /> */}
+      </React.Fragment>
     )
   }
 }
