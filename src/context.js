ca134e996e9c62ee6dd9ff0d6cab010d2dbb73d9
PhreetechDev4 <gloriaojuks@gmail.com>
2019-05-03 18:06:45 +0100

    worked on the productList setup

---
+++ src/context.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import {storeProducts, detailProduct} from './data'
+
+
+const ProductContext = React.createContext();
+//provider
+
+// consumer
+
+ class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detailProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log('hello from detail')
+    }
+
+    addToCart = () => { 
+        console.log('hello from cart')
+    }
+  render() {
+    return (
+      <ProductContext.Provider value={{
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart
+      }}>
+          {this.props.children}
+      </ProductContext.Provider>
+
+
+    )
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer};
+
+
