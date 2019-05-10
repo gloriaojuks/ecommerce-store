f6389b23f373272df0d36d001a910e9a07fa07cd
PhreetechDev4 <gloriaojuks@gmail.com>
2019-05-10 18:33:12 +0100

    cleanning up

--- src/data.js
+++ src/data.js
@@ -33,7 +33,7 @@ export const storeProducts = [
     title: "Gape Gown - Black",
     img: "img/product-3.jpg",
     price: 8,
-    company: "htc",
+    company: "Next Woman",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
