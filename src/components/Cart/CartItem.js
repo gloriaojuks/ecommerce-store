9be2211f38b9cbf92626f3d57b0ca154ad3c59cc
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-05-10 05:33:30 +0100

    removed component errors

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-export default function CartItem({item,value}) {
-    const{id,title,img,price,total,count} = item;
-  return (
-    <div>
-      helo
-    </div>
-  )
+export default function CartItem({ item, value }) {
+	console.log(item);
+	const { id, title, img, price, total, count } = item;
+	return (
+		<div>
+			<ul>
+				<li>product ID: {id}</li>
+				<li>product title: {title}</li>
+				<li>product img: {img}</li>
+				<li>product price: {price}</li>
+				<li>product total: {total}</li>
+				<li>product count: {count}</li>
+			</ul>
+		</div>
+	);
 }
-
-
-
- 
--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import CartItem from './CartItem'
+import React from 'react';
+import CartItem from './CartItem';
 
-export default function CartList({value}) {
-    const {cart} = value
-  
-    return (
-    <div className="comtainer-fluid">
-        {cart.map(item=>{
-            return <CartItem key={item.id} item={item} value={value} />
-        })}
-        <CartItem />
-    </div>
-    );
+export default function CartList({ value }) {
+	const { cart } = value;
+
+	return (
+		<div className="comtainer-fluid">
+			{cart.map((item, index) => {
+				if (item.count > 0) {
+					return <CartItem key={item.id} item={item} value={value} />;
+				}
+			})}
+
+			{/* <CartItem /> your not ment to render CartItem without prop */}
+		</div>
+	);
 }
 
+//you were redering <CartItem /> outside the map without passing props
