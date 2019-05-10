0511650abb60a7beee4f7f1c99dd63d527df0657
PhreetechDev4 <gloriaojuks@gmail.com>
2019-05-10 20:26:35 +0100

    empty cart update

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import PayPalButton from './PayPalButton'
 
-export default function CartTotals({value}) {
+export default function CartTotals({value,history}) {
     const{cartSubTotal,cartTax,cartTotal,clearCart}=value
   return (
         <React.Fragment>
@@ -23,6 +24,7 @@ export default function CartTotals({value}) {
                             <span className="text-title">Total :</span>
                             <strong>$ {cartTotal}</strong>
                         </h5>
+                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
                     </div>
                 </div>
             </div>
--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -4,7 +4,9 @@ export default function EmptyCart() {
   return (
     <div className="container mt-5">
         <div className="row">
-            <div className="col-10 "></div>
+            <div className="col-10 ">
+                <h1 style={{textAlign:'center'}}>Your Cart is Currently Empty</h1>
+            </div>
         </div>
     </div>
   )
